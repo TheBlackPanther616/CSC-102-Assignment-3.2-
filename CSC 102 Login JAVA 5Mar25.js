a2d7860276dc0daf62ae924a0419d9ec07b08323
TheBlackPanther616 <josephhudsonjr13@gmail.com>
2025-03-06 10:25:54 -0600

    Add files via upload

--- CSC 102 Login JAVA 5Mar25.js
+++ CSC 102 Login JAVA 5Mar25.js
@@ -3,12 +3,22 @@ function checkCreds()
     var firstName = document.getElementById("fName").value;
     var lastName = document.getElementById("lName").value;
     var fullName = firstName +  " " + lastName;
+    var zipNum = document.getElementById("zcode").value;
     var badgeNum = document.getElementById("badgeID").value;
     var passW = document.getElementById("PW").value;
 
     if (fullName.length <= 1)
     {
         document.getElementById("loginStatus").innerHTML = "Names must have values";
+    }
+    else if (zipNum.length !=5)
+        {
+            document.getElementById("loginStatus").innerHTML = "Invalid zip code";
+        }
+
+    else if ((zipNum<10000)  || (zipNum>99999))
+    {
+        document.getElementById("loginStatus").innerHTML = "Invalid zip code";
     }
 // this is redundant - just shows another way to check badgenum
     else if (badgeNum.length !=3)
