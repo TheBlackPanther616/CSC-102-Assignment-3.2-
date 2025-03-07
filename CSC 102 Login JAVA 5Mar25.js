23e55fcef838e193a40e7714e6c80164b9d844c4
TheBlackPanther616 <josephhudsonjr13@gmail.com>
2025-03-07 17:32:05 -0600

    Add files via upload

--- CSC 102 Login JAVA 5Mar25.js
+++ CSC 102 Login JAVA 5Mar25.js
@@ -4,30 +4,30 @@ function checkCreds()
     var firstName = document.getElementById("fName").value;
     var lastName = document.getElementById("lName").value;
     var fullName = firstName +  " " + lastName;
-    var zipNum = document.getElementById("zcode").value;
+    var zipNum = document.getElementById("zipCode").value;
     var badgeNum = document.getElementById("badgeID").value;
     var passW = document.getElementById("PW").value;
+ 
+    
 
     // Establishing arguements for inputs
     if (fullName.length <= 1)
     {
-        document.getElementById("loginStatus").innerHTML = "Names must have values";
+        document.getElementById("loginStatus").innerHTML = "Names must have more than one Character";
     }
-    else if (zipNum.length !=5)
+    else if (fullName.length >= 20)
+        {
+            document.getElementById("loginStatus").innerHTML = "Names has too many Characters";
+        }
+        else if (zipNum.length !=5)
+            {
+                document.getElementById("loginStatus").innerHTML = "Invalid zip code";
+            }
+    
+        else if ((zipNum<10000)  || (zipNum>99999))
         {
             document.getElementById("loginStatus").innerHTML = "Invalid zip code";
         }
-
-    else if ((zipNum<10000)  || (zipNum>99999))
-    {
-        document.getElementById("loginStatus").innerHTML = "Invalid zip code";
-    }
-// this is redundant - just shows another way to check badgenum
-    else if (badgeNum.length !=3)
-        {
-            document.getElementById("loginStatus").innerHTML = "Invalid badge num - 1ST CHECK W LEN";
-        }
-
     else if ((badgeNum<100)  || (badgeNum>999))
     {
         document.getElementById("loginStatus").innerHTML = "Invalid badge num";
